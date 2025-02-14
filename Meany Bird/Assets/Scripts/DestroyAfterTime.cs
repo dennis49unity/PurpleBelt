7c6164588baec33b640db2165700979f0380d185
bbobjedi <dennislittlecat@outlook.com>
2025-02-14 17:05:21 -0500

    h
    jhhj

--- Meany Bird/Assets/Scripts/DestroyAfterTime.cs
+++ Meany Bird/Assets/Scripts/DestroyAfterTime.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class DestroyAfterTime : MonoBehaviour
 {
+    [Header("Default Destruction Time")]
+    public float timeToDestruction;
     // Start is called before the first frame update
     void Start()
     {
