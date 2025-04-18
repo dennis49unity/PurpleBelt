fdcc2552e05e1534cfd56fd13464d1bc08194c11
bbobjedi <dennislittlecat@outlook.com>
2025-04-18 16:46:19 -0400

    d
    sdgf

--- Dont Touch the Cubes/Assets/Scripts/PlayerControls.cs
+++ Dont Touch the Cubes/Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Rigidbody")]
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+        Time.timeScale = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        transform.rotation *= Quaternion.Euler(0, 0, 7 * Time.deltaTime);
+        Time.timeScale += Time.fixedDeltaTime * 0.01f;
 
+        rb.velocity += transform.rotation * (Vector3.right * Input.GetAxisRaw("Horizontal") * 10f * Time.deltaTime);
+        rb.velocity += transform.rotation * (Vector3.up * Input.GetAxisRaw("Vertical") * 10f * Time.deltaTime);
+
+        float clampedX = Mathf.Clamp(transform.position.x, -30f, 30f);
+        float clampedZ = Mathf.Clamp(transform.position.z, -30f, 30f);
+
+        transform.position = new Vector3(clampedX, 0, clampedZ);
     }
 }
--- Dont Touch the Cubes/Assets/Scripts/SpawnObjects.cs
+++ Dont Touch the Cubes/Assets/Scripts/SpawnObjects.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
+    [Header("Spawn Cube Object")]
+    public GameObject spawnCube;
+    [Header("Default Difficulty")]
+    punlic float difficulty = 40f;
+    float spawn;
 
     // Update is called once per frame
     void Update()
     {
-
+        spawn -= 1;
+        Vector3 v3Pos = transform.position + new Vector3(Random.value * 40f - 20f, 0, Random.value * 40f - 20f);
+        Quaternion qRotation = Quarternion.Euler(0, Random.value * 360f, Random.value * 30f);
+        GameObject createObject = Instantiate(spawnCube, v3Pos, qRotation);
     }
 }
