09f2a57c871ef45b816b1ba9012c1a1489c04e71
idkwhatimdealingwith <quinnsidell@icloud.com>
2025-05-23 16:44:15 -0400

    s
    s

---
+++ Polyrun/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Default Jumping Power")]
+    public float jumpPower = 6f;
+    [Header("Boolean isGrounded")]
+    public bool isGrounded;
+    float posX = 0.0f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        posX = transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpPower * rb.mass * rb.gravityScale * 20f);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+    
+}
+    
+
